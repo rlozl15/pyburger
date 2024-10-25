c27c7c31fc3eff94e4134b2c10e81f28bee73a5a
rlozl15 <eunjikim.dev@gmail.com>
2024-10-26 00:55:54 +0900

    feat: Django 프로젝트 생성 및 View 사용하기
    - Docker를 통해 Django 프로젝트 생성
    - 간단한 View 사용해보기

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def main(request):
+    return HttpResponse("안녕하세요, pyburger입니다.")
+
+def burger_list(request):
+    return HttpResponse("pyburger의 햄버거 목록입니다.")
+
