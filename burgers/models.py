f2ca7b6ecfd5ea791ba20556efbe81bd40ef05f7
rlozl15 <eunjikim.dev@gmail.com>
2024-10-26 01:59:18 +0900

    feat: Django admin 페이지 생성 및 Burger 데이터 추가

--- burgers/models.py
+++ burgers/models.py
@@ -7,3 +7,6 @@ class Burger(models.Model):
     name = models.CharField(max_length = 20)
     price = models.IntegerField(default=0)
     calories = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
