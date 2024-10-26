cb9b5bcf3547345a00d824b1b32f47cc844b89ed
rlozl15 <eunjikim.dev@gmail.com>
2024-10-26 23:47:53 +0900

    feat: view를 통해 데이터를 template으로 보내 데이터를 보여주는 기능 개발
    - render 함수를 통해 템플릿으로 객체 전달
    - jinja2를 통해 버거 목록 보여주기

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
     return render(request, "main.html")
 
 def burger_list(request):
-    return render(request, "burger_list.html")
+    burgers = Burger.objects.all()
+    context = {
+        "burgers":burgers,
+    }
+    return render(request, "burger_list.html", context)
 
