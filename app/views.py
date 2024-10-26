462729d653ff4f6c7d3af0257b9361fda17e6cf5
rlozl15 <eunjikim.dev@gmail.com>
2024-10-27 00:25:15 +0900

    feat: 햄버거 검색 기능
    - django의 orm 기능 중 filter를 통해 keyword 검색
    - GET방식으로 form 데이터를 전송

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 from burgers.models import Burger
 
@@ -11,3 +13,17 @@ def burger_list(request):
     }
     return render(request, "burger_list.html", context)
 
+def burger_search(request):
+    keyword = request.GET.get("keyword")
+
+    # keyword가 주어진 경우
+    if keyword:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    # keyword가 없는 경우, 즉 주소표시줄에 keyword가 없어 None을 반환한 경우
+    else:
+        burgers = Burger.objects.none()
+
+    contexts = {
+        "burgers": burgers,
+    }
+    return render(request, "burger_search.html", contexts)
